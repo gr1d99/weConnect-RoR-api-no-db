bc5914438aab9ef330f8fe60233bda93ff62a17b
Gideon Kimutai <gideonkimutai9@gmail.com>
2018-07-04 19:00:00 +0300

    fix(categoryModel): remove unecessary validation module
    [Fixes #158824865]

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,31 +2,22 @@ require 'spec_helper'
 require 'rails_helper'
 
 RSpec.describe Category do
-  let!(:category) { Category.new }
+  let!(:detail) { { name: 'Business' }}
+  let!(:category) { Category.new(detail) }
 
   context 'when name is provided' do
-    let!(:valid_category) do
-      category.name = 'Business'
-      category
-    end
-
-    it 'is valid' do
-      expect(valid_category.valid?).to be_truthy
-    end
-
     it 'has id' do
-      expect(valid_category.id).not_to be_nil
+      expect(category.id).not_to be_nil
+    end
+
+    it 'has given name' do
+      expect(category.name).to eq(detail[:name])
     end
   end
 
   context 'when name is not provided' do
-    let!(:invalid_category) do
-      category.name = nil
-      category
-    end
-
-    it 'is invalid' do
-      expect(category.valid?).to be_falsey
+    it 'raises an exception' do
+      expect{ Category.new }.to raise_exception(ArgumentError)
     end
   end
 end
