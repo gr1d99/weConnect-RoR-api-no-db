c11eb4b9348cb818fdfb19409dbe5c6595cf62c8
Gideon Kimutai <gideonkimutai9@gmail.com>
2018-07-05 10:51:23 +0300

    chore(codeClimate): update code style check config
    [Finishes #158832829]

--- app/models/category.rb
+++ app/models/category.rb
@@ -10,5 +10,3 @@ class Category
     @name = name
   end
 end
-
-
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -4,27 +4,31 @@ require 'spec_helper'
 RSpec.describe Location do
   let!(:details) do
     {
-        city: 'Nairobi',
-        email: 'business@gmail.com',
-        country: 'Kenya',
-        street: 'Tom Mboya 118',
-        state: 'Nairobi',
-        zip_code: '1202',
-        phone_number: '07**********'
+      city: 'Nairobi',
+      email: 'business@gmail.com',
+      country: 'Kenya',
+      street: 'Tom Mboya 118',
+      state: 'Nairobi',
+      zip_code: '1202',
+      phone_number: '07**********'
     }
   end
 
   context 'instance methods' do
-    let!(:actual_methods) { %i(id state email country street city zip_code phone_number ) }
+    let!(:actual_methods) do
+      %i[id state email country street city zip_code phone_number]
+    end
+
     let!(:expected_methods) do
-      Location.public_instance_methods(false).select! { |method| method unless method.to_s.end_with? '=' }
+      Location.public_instance_methods(false)
+              .select! { |method| method unless method.to_s.end_with? '=' }
     end
 
     it 'has all expected methods' do
       expect(expected_methods.empty?).not_to be_truthy
       expect(expected_methods.length).to eq(actual_methods.length)
       actual_methods.each do |method|
-        expect(expected_methods.include? method).to be_truthy
+        expect(expected_methods.include?(method)).to be_truthy
       end
     end
   end
