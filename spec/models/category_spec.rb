aa62336ddf2c467d6993c5e4bf9d7271bb31caa3
Gideon Kimutai <gideonkimutai9@gmail.com>
2018-07-09 09:46:12 +0300

    Revert "feat(businessCategory): enable creation of categories (#9)"
    This reverts commit f211368391155ddcc1cecdf1a6a36d1777a11138.

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -6,10 +6,6 @@ RSpec.describe Category do
   let!(:category) { Category.new(detail) }
 
   context 'when name is provided' do
-    it 'creates a new category' do
-      expect(Category.create(detail)).to be_an_instance_of(Category)
-    end
-
     it 'has id' do
       expect(category.id).not_to be_nil
     end
@@ -22,19 +18,6 @@ RSpec.describe Category do
   context 'when name is not provided' do
     it 'raises an exception' do
       expect { Category.new }.to raise_exception(ArgumentError)
-    end
-  end
-
-  context 'when name is blank' do
-    let!(:category) { Category.new(name: nil) }
-    let!(:error_message) { "Name can't be blank" }
-
-    it 'adds errors to errors list' do
-      expect(category.errors.length).to eq(1)
-    end
-
-    it 'returns correct error message' do
-      expect(category.errors).to include(error_message)
     end
   end
 end
