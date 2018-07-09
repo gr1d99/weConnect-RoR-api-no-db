a25daac9963ae549ff19a30a3a0af37cbbaa5497
Gideon Kimutai <gideonkimutai9@gmail.com>
2018-07-09 09:09:48 +0300

    feat(businessCategory): enable creation of categories
    [Delivers #158900526]

---
+++ app/controllers/category_controller.rb
@@ -0,0 +1,23 @@
+require 'categories'
+
+CATEGORIES ||= Categories.new
+
+class CategoryController < ApplicationController
+  def create
+    category = Category.create(category_params[:name])
+    if category.has_errors
+      errors = { errors: category.errors }
+      render json: errors, status: :unprocessable_entity
+    else
+      CATEGORIES.add(category)
+      payload = { id: category.id, name: category.name }
+      render json: payload, status: :created
+    end
+  end
+
+  private
+
+  def category_params
+    params.permit(:name)
+  end
+end
